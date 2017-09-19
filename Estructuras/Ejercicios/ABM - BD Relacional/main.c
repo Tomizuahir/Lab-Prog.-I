4519702807215d59c6257c37ed10a083abeaa656
elisca <ezequiel.lisca@live.com>
2017-09-18 22:06:44 -0300

    Clase 18/09/2018
    Bases de Datos Relacionales (Uno a Varios). Programas ABM.

---
+++ Estructuras/Ejercicios/ABM - BD Relacional/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void hardcodearProgramadores(eProgramador[],int);
+
+typedef struct
+{
+    int idProg;
+    char nombre[64];
+    int cat;
+    int proyecto;
+    float sueldo;
+    int cant_hs;
+}eProgramador;
+
+typedef struct
+{
+    int id;
+    char descripcion;
+    int pago_hora;
+}eCategoria;
+
+int main()
+{
+
+}
+
+void hardcodearProgramadores(eProgramador listaProg[],int tamListaProg)
+{
+    listaProg[0]={0,"ANA",0,}
+}
